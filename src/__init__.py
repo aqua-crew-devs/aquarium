02856648b55db1cf603a775955fa3e71775c5a3e
Qingyu Deng <bitdqy@hotmail.com>
2019-12-29 00:24:43 +0800

    feat: add app

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
+from os import makedirs
 from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__, instance_path="/aquarium")
+
+    makedirs(app.instance_path, exist_ok=True)
+
+    return app
