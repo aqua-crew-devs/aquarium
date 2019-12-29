74a815c4ebbe566060b111d8ff9279e29a39573c
Qingyu Deng <bitdqy@hotmail.com>
2019-12-29 11:32:32 +0800

    test: setup fixture

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,9 @@ from os import makedirs
 from flask import Flask
 
 
-def create_app():
+def create_app(test_config):
     app = Flask(__name__, instance_path="/aquarium")
-
+    if test_config:
+        app.config.from_mapping(test_config)
     makedirs(app.instance_path, exist_ok=True)
-
     return app
---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src import create_app
+
+
+@pytest.fixture
+def app():
+    test_config = {"TESTING": True}
+    app = create_app(test_config)
+
+    return app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
