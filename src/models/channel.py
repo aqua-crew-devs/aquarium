cb7a48701920650bfa4f9a36611fa7b92a00a394
Qingyu Deng <bitdqy@hotmail.com>
2020-01-25 18:42:27 +0800

    feat: add save method

--- src/models/channel.py
+++ src/models/channel.py
@@ -1,21 +1,32 @@
 from datetime import datetime
 from .database import get_mongo_client
 
+
 class Channel:
-    def __init__(self, id: str, name: str, description: str, thumbnail: str, published_at: datetime, country: str):
+    def __init__(
+        self,
+        id: str,
+        name: str,
+        description: str,
+        thumbnail: str,
+        published_at: datetime,
+        country: str,
+    ):
         self.id = id
         self.name = name
-        self.description=description
-        self.thumbnail=thumbnail
-        self.published_at=published_at
-        self.country=country
+        self.description = description
+        self.thumbnail = thumbnail
+        self.published_at = published_at
+        self.country = country
 
     def __eq__(self, other):
         return vars(self) == vars(other)
+
 
 class ChannelManager:
     @staticmethod
     def save(channel: Channel):
         channels = get_mongo_client().aquarium.channels
-        
-        channels.insert_one(vars(channel))
+
+        channels.find_one_and_replace({"id": channel.id}, vars(channel), upsert=True)
+
--- tests/test_models/test_channel.py
+++ tests/test_models/test_channel.py
@@ -34,3 +34,36 @@ def test_it_should_create_a_channel_if_no_such_channel_is_presented(app):
         assert channel["thumbnail"] == "https://url_to_thumbnails"
         assert channel["published_at"] == datetime(2020, 1, 24, 6, 14, 00)
         assert channel["country"] == "JP"
+
+
+def test_it_should_update_channel_if_channel_is_presented(app):
+    with app.app_context():
+        channels = pymongo.MongoClient(*TEST_MONGO_SERVER).aquarium.channels
+        channels.insert_one(
+            {
+                "id": "abcd",
+                "name": "helloworld",
+                "description": "an awesome channel",
+                "thumbnail": "https://url_to_thumbnails",
+                "published_at": datetime(2020, 1, 24, 6, 14, 00),
+                "country": "JP",
+            }
+        )
+
+        ChannelManager.save(
+            Channel(
+                **{
+                    "id": "abcd",
+                    "name": "new channel name",
+                    "description": "an awesome channel",
+                    "thumbnail": "https://url_to_thumbnails",
+                    "published_at": datetime(2020, 1, 24, 6, 14, 00),
+                    "country": "JP",
+                }
+            )
+        )
+
+        channel = channels.find_one({"id": "abcd"})
+        assert channel is not None
+        assert channel["id"] == "abcd"
+        assert channel["name"] == "new channel name"
